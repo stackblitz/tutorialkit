d9698806b6007dd964267bb03eb949efdc53b81e
Dominic Elm <elmdominic@gmx.net>
2024-05-09 19:46:25 +0200

    feat: memoize preview panel

--- template/src/components/internal/PreviewPanel.tsx
+++ template/src/components/internal/PreviewPanel.tsx
@@ -3,7 +3,7 @@ import type { PreviewInfo } from '@components/webcontainer/preview-info';
 import { useStore } from '@nanostores/react';
 import resizePanelStyles from '@styles/resize-panel.module.css';
 import classnames from 'classnames';
-import { createElement, forwardRef, useContext, useImperativeHandle } from 'react';
+import { createElement, forwardRef, memo, useContext, useImperativeHandle } from 'react';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 import { TutorialRunnerContext } from '../webcontainer/tutorial-runner';
 
@@ -15,79 +15,81 @@ export type ImperativePreviewHandle = {
   reload: () => void;
 };
 
-export const PreviewPanel = forwardRef<ImperativePreviewHandle, Props>(({ toggleTerminal }, ref) => {
-  const tutorialRunner = useContext(TutorialRunnerContext);
-  const expectedPreviews = useStore(tutorialRunner.previews);
+export const PreviewPanel = memo(
+  forwardRef<ImperativePreviewHandle, Props>(({ toggleTerminal }, ref) => {
+    const tutorialRunner = useContext(TutorialRunnerContext);
+    const expectedPreviews = useStore(tutorialRunner.previews);
 
-  const hasPreviews = expectedPreviews.some((preview) => preview.ready);
+    const hasPreviews = expectedPreviews.some((preview) => preview.ready);
 
-  useImperativeHandle(
-    ref,
-    () => ({
-      reload: () => {
-        // can't use a ref because PanelGroup does not expose the underlying html element
-        const previewPanel = document.getElementById('preview-panel');
+    useImperativeHandle(
+      ref,
+      () => ({
+        reload: () => {
+          // can't use a ref because PanelGroup does not expose the underlying html element
+          const previewPanel = document.getElementById('preview-panel');
 
-        if (previewPanel) {
-          const iframes = previewPanel.querySelectorAll('iframe');
+          if (previewPanel) {
+            const iframes = previewPanel.querySelectorAll('iframe');
 
-          for (const iframe of iframes) {
-            iframe.src = iframe.src;
+            for (const iframe of iframes) {
+              iframe.src = iframe.src;
+            }
           }
-        }
-      },
-    }),
-    [],
-  );
+        },
+      }),
+      [],
+    );
 
-  if (!hasPreviews) {
-    return (
-      <div className="h-full w-full">
-        <div className="panel-header border-y border-tk-elements-app-borderColor justify-between">
-          <div className="panel-title">
-            <div className="panel-icon i-ph-lightning-duotone"></div>
-            <span className="text-sm">Preparing Environment</span>
+    if (!hasPreviews) {
+      return (
+        <div className="h-full w-full">
+          <div className="panel-header border-y border-tk-elements-app-borderColor justify-between">
+            <div className="panel-title">
+              <div className="panel-icon i-ph-lightning-duotone"></div>
+              <span className="text-sm">Preparing Environment</span>
+            </div>
+            <button
+              className="panel-button px-2 py-0.5 -mr-1 -my-1"
+              title="Toggle Terminal"
+              onClick={() => toggleTerminal?.()}
+            >
+              <span className="panel-button-icon i-ph-terminal-window-duotone"></span>
+              <span className="text-sm">Toggle Terminal</span>
+            </button>
           </div>
-          <button
-            className="panel-button px-2 py-0.5 -mr-1 -my-1"
-            title="Toggle Terminal"
-            onClick={() => toggleTerminal?.()}
-          >
-            <span className="panel-button-icon i-ph-terminal-window-duotone"></span>
-            <span className="text-sm">Toggle Terminal</span>
-          </button>
+          <BootScreen />
         </div>
-        <BootScreen />
-      </div>
-    );
-  }
-
-  const previews = expectedPreviews.filter((preview) => preview.ready);
-  const defaultSize = 100 / previews.length;
-  const minSize = 20;
-
-  const children = [];
-
-  for (const [index, preview] of previews.entries()) {
-    children.push(
-      <Panel defaultSize={defaultSize} minSize={minSize}>
-        <Preview
-          preview={preview}
-          previewCount={previews.length}
-          first={index === 0}
-          last={index === previews.length - 1}
-          toggleTerminal={toggleTerminal}
-        />
-      </Panel>,
-    );
-
-    if (index !== previews.length - 1) {
-      children.push(<PanelResizeHandle className={resizePanelStyles.PanelResizeHandle} />);
+      );
     }
-  }
 
-  return createElement(PanelGroup, { id: 'preview-panel', direction: 'horizontal' }, ...children);
-});
+    const previews = expectedPreviews.filter((preview) => preview.ready);
+    const defaultSize = 100 / previews.length;
+    const minSize = 20;
+
+    const children = [];
+
+    for (const [index, preview] of previews.entries()) {
+      children.push(
+        <Panel defaultSize={defaultSize} minSize={minSize}>
+          <Preview
+            preview={preview}
+            previewCount={previews.length}
+            first={index === 0}
+            last={index === previews.length - 1}
+            toggleTerminal={toggleTerminal}
+          />
+        </Panel>,
+      );
+
+      if (index !== previews.length - 1) {
+        children.push(<PanelResizeHandle className={resizePanelStyles.PanelResizeHandle} />);
+      }
+    }
+
+    return createElement(PanelGroup, { id: 'preview-panel', direction: 'horizontal' }, ...children);
+  }),
+);
 
 interface PreviewProps {
   preview: PreviewInfo;
