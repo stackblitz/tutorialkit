e044611ef3072586cc5f91f4c70d3d3fc29f077d
Jòan <Nemikolh@users.noreply.github.com>
2024-05-16 16:50:12 +0000

    fix: adjust wording on BinaryContent component

--- template/src/components/CodeMirrorEditor/BinaryContent.tsx
+++ template/src/components/CodeMirrorEditor/BinaryContent.tsx
@@ -1,7 +1,7 @@
 export function BinaryContent() {
   return (
     <div className="flex items-center justify-center absolute inset-0 z-10 bg-tk-elements-app-backgroundColor">
-      Binary content is not supported
+      File format cannot be displayed.
     </div>
   );
 }
