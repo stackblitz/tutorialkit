c21def169cf00c23adebc38d9bcfd26b45e1e5f6
Jòan <Nemikolh@users.noreply.github.com>
2024-05-23 12:27:49 +0000

    fix: unused folders

--- packages/template/src/schemas/chapter.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'astro:content';
-import { baseSchema } from './common';
-
-export const chapterSchema = baseSchema.extend({
-  type: z.literal('chapter'),
-});
-
-export type ChapterSchema = z.infer<typeof chapterSchema>;
--- packages/template/src/schemas/part.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'astro:content';
-import { baseSchema } from './common';
-
-export const partSchema = baseSchema.extend({
-  type: z.literal('part'),
-});
-
-export type PartSchema = z.infer<typeof partSchema>;
