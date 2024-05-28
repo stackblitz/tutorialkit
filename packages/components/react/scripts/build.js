7126321d9b30603e719727228915d1c11aaaa2e5
Jòan <Nemikolh@users.noreply.github.com>
2024-05-28 19:11:12 +0000

    fix: use globSync

--- packages/components/react/scripts/build.js
+++ packages/components/react/scripts/build.js
@@ -1,13 +1,13 @@
 import { cp } from 'fs/promises';
 import { spawnSync } from 'child_process';
 import { join } from 'path';
-import glob from 'fast-glob';
+import fastGlob from 'fast-glob';
 
 // build everything with typescript
 spawnSync('tsc', ['-b'], { stdio: 'inherit' });
 
 // copy css files unmodified
-const filePaths = await glob(`./src/**/*.css`, {
+const filePaths = fastGlob.globSync(`./src/**/*.css`, {
   onlyFiles: true,
 });
 
