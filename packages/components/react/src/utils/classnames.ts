d1cbffdde4241c24f6ac1684e49866542ba66e11
Jòan <Nemikolh@users.noreply.github.com>
2024-05-28 19:08:52 +0000

    fix: add tsdocs to classnames and copyright

--- packages/components/react/src/utils/classnames.ts
+++ packages/components/react/src/utils/classnames.ts
@@ -1,5 +1,18 @@
+/*!
+  Copyright (c) 2018 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+
 type ClassNamesArg = undefined | string | Record<string, boolean> | ClassNamesArg[];
 
+/**
+ * A simple JavaScript utility for conditionally joining classNames together.
+ *
+ * @param args a series of classes or object with key that are class and values
+ *             that are interpreted as boolean to decide whether or not the class
+ *             should be included in the final class
+ */
 export function classNames(...args: ClassNamesArg[]): string {
   let classes = '';
 
