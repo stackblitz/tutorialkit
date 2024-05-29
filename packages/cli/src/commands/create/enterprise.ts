652c4ba2c56bb1c37c8e42f1195ea4035db781d3
Jòan <Nemikolh@users.noreply.github.com>
2024-05-29 18:50:29 +0000

    fix: remove enterprise question from CLI

--- packages/cli/src/commands/create/enterprise.ts
+++ packages/cli/src/commands/create/enterprise.ts
@@ -1,7 +1,5 @@
-import * as prompts from '@clack/prompts';
 import fs from 'node:fs';
 import path from 'node:path';
-import { assertNotCanceled } from '../../utils/tasks.js';
 import type { CreateOptions } from './options.js';
 import { parseAstroConfig, replaceArgs } from './astro-config.js';
 import { generate } from './babel.js';
@@ -10,26 +8,7 @@ export async function setupEnterpriseConfig(dest: string, flags: CreateOptions) 
   let editorOrigin = flags.enterprise;
 
   if (!flags.defaults && flags.enterprise === undefined) {
-    const answer = await prompts.confirm({
-      message: `TutorialKit uses StackBlitz WebContainers, do you want to configure the Enterprise version?`,
-      initialValue: false,
-    });
-
-    assertNotCanceled(answer);
-
-    if (!answer) {
-      return;
-    }
-
-    const editorURL = await prompts.text({
-      message: `What's the origin of your StackBlitz instance?`,
-      placeholder: 'https://editor.stackblitz.acme.com',
-      validate: validateEditorOrigin,
-    });
-
-    assertNotCanceled(editorURL);
-
-    editorOrigin = new URL(editorURL).origin;
+    return;
   } else if (editorOrigin) {
     const error = validateEditorOrigin(editorOrigin);
 
