693d4a4a50f0c825359df0e42e62c8625aa55dd4
Jòan <Nemikolh@users.noreply.github.com>
2024-05-29 18:37:36 +0000

    feat: load user provided css for theme customization

--- packages/astro/src/types.ts
+++ packages/astro/src/types.ts
@@ -4,4 +4,5 @@ export type AstroConfigSetupOptions = Parameters<NonNullable<AstroIntegration['h
 export type AstroServerSetupOptions = Parameters<Required<AstroIntegration['hooks']>['astro:server:setup']>['0'];
 export type AstroBuildDoneOptions = Parameters<Required<AstroIntegration['hooks']>['astro:build:done']>['0'];
 export type ViteDevServer = AstroServerSetupOptions['server'];
+export type VitePlugin = ViteDevServer['config']['plugins'][0];
 export type Files = Record<string, string | { base64: string }>;
