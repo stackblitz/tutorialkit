ab3554147c53eb53cff335bf1c7db9a68d2e5f4b
Jòan <Nemikolh@users.noreply.github.com>
2024-05-30 22:37:03 +0000

    fix: actually fix the previews bug

--- packages/components/react/src/Panels/PreviewPanel.tsx
+++ packages/components/react/src/Panels/PreviewPanel.tsx
@@ -3,13 +3,17 @@ import type { PreviewInfo, TutorialRunner } from '@tutorialkit/runtime';
 import { useStore } from '@nanostores/react';
 import resizePanelStyles from '../styles/resize-panel.module.css';
 import { classNames } from '../utils/classnames.js';
-import { createElement, forwardRef, memo, useImperativeHandle } from 'react';
+import { createElement, forwardRef, memo, useEffect, useImperativeHandle, useRef } from 'react';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
 interface Props {
   toggleTerminal?: () => void;
   tutorialRunner: TutorialRunner;
 }
+
+const previewsContainer = globalThis.document ? document.getElementById('previews-container')! : ({} as HTMLElement);
+
+type IframeRef = { ref: HTMLIFrameElement | undefined };
 
 export type ImperativePreviewHandle = {
   reload: () => void;
@@ -18,8 +22,10 @@ export type ImperativePreviewHandle = {
 export const PreviewPanel = memo(
   forwardRef<ImperativePreviewHandle, Props>(({ toggleTerminal, tutorialRunner }, ref) => {
     const expectedPreviews = useStore(tutorialRunner.previews);
+    const iframeRefs = useRef<IframeRef[]>([]);
 
-    const hasPreviews = expectedPreviews.some((preview) => preview.ready);
+    const activePreviewsCount = expectedPreviews.reduce((count, preview) => (preview.ready ? count + 1 : count), 0);
+    const hasPreviews = activePreviewsCount > 0;
 
     useImperativeHandle(
       ref,
@@ -40,9 +46,19 @@ export const PreviewPanel = memo(
       [],
     );
 
+    adjustSize(iframeRefs.current, activePreviewsCount, newIframeRef);
+    preparePreviewsContainer(activePreviewsCount);
+
+    // update preview refs
+    for (const [index, iframeRef] of iframeRefs.current.entries()) {
+      if (!iframeRef.ref) {
+        iframeRef.ref = previewsContainer.children.item(index) as HTMLIFrameElement;
+      }
+    }
+
     if (!hasPreviews) {
       return (
-        <div className="h-full w-full">
+        <div className="flex flex-col h-full w-full">
           <div className="panel-header border-y border-tk-elements-app-borderColor justify-between">
             <div className="panel-title">
               <div className="panel-icon i-ph-lightning-duotone"></div>
@@ -72,6 +88,7 @@ export const PreviewPanel = memo(
       children.push(
         <Panel defaultSize={defaultSize} minSize={minSize}>
           <Preview
+            iframe={iframeRefs.current[index]}
             preview={preview}
             previewCount={previews.length}
             first={index === 0}
@@ -91,6 +108,7 @@ export const PreviewPanel = memo(
 );
 
 interface PreviewProps {
+  iframe: IframeRef;
   preview: PreviewInfo;
   previewCount: number;
   first?: boolean;
@@ -98,7 +116,40 @@ interface PreviewProps {
   toggleTerminal?: () => void;
 }
 
-function Preview({ preview, previewCount, first, last, toggleTerminal }: PreviewProps) {
+function Preview({ preview, iframe, previewCount, first, last, toggleTerminal }: PreviewProps) {
+  const previewContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!iframe.ref) {
+      return;
+    }
+
+    if (preview.url) {
+      iframe.ref.src = preview.url;
+    }
+  }, [preview.url, iframe.ref]);
+
+  useEffect(() => {
+    const padding = 1;
+    const resizeObserver = new ResizeObserver(() => {
+      if (!iframe.ref) {
+        return;
+      }
+
+      const { left, top, width, height } = previewContainerRef.current?.getBoundingClientRect()!;
+      iframe.ref.style.left = `${Math.floor(left) - padding}px`;
+      iframe.ref.style.top = `${Math.floor(top) - padding}px`;
+      iframe.ref.style.height = `${Math.floor(height) + padding}px`;
+      iframe.ref.style.width = `${Math.floor(width) + padding}px`;
+    });
+
+    resizeObserver.observe(previewContainerRef.current!);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
   return (
     <div className="panel-container">
       <div
@@ -122,17 +173,11 @@ function Preview({ preview, previewCount, first, last, toggleTerminal }: Preview
         )}
       </div>
       <div
+        ref={previewContainerRef}
         className={classNames('h-full w-full flex justify-center items-center', {
           'border-l border-tk-elements-previews-borderColor': !first,
         })}
-      >
-        <iframe
-          title="Preview Window"
-          src={preview.url}
-          className="h-full w-full"
-          allow="magnetometer; accelerometer; gyroscope; geolocation; microphone; camera; payment; autoplay; serial; xr-spatial-tracking; cross-origin-isolated"
-        />
-      </div>
+      />
     </div>
   );
 }
@@ -147,4 +192,39 @@ function previewTitle(preview: PreviewInfo, previewCount: number) {
   }
 
   return `Preview on port ${preview.port}`;
+}
+
+function preparePreviewsContainer(previewCount: number) {
+  while (previewsContainer.childElementCount < previewCount) {
+    const iframe = document.createElement('iframe');
+    iframe.className = 'absolute -z-10';
+    iframe.allow =
+      'magnetometer; accelerometer; gyroscope; geolocation; microphone; camera; payment; autoplay; serial; xr-spatial-tracking; cross-origin-isolated';
+
+    previewsContainer.appendChild(iframe);
+  }
+
+  for (let i = 0; i < previewsContainer.childElementCount; i++) {
+    const preview = previewsContainer.children.item(i)!;
+
+    if (i < previewCount) {
+      preview.classList.remove('hidden');
+    } else {
+      preview.classList.add('hidden');
+    }
+  }
+}
+
+function newIframeRef(): IframeRef {
+  return { ref: undefined };
+}
+
+function adjustSize<T>(array: T[], expectedSize: number, newElement: () => T) {
+  while (array.length < expectedSize) {
+    array.push(newElement());
+  }
+
+  while (array.length > expectedSize) {
+    array.pop();
+  }
 }
