600f4dfc7d30ef40994bdc1b77723cff1574120e
Dominic Elm <elmdominic@gmx.net>
2024-06-05 21:05:22 +0200

    fixup: code review

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,4 @@
 # if no tag is specified we'll use 'latest' as the default
 TAG=${1:-latest}
 
-pnpm build && pnpm publish --recursive --tag "$TAG" --filter "@tutorialkit/*" --filter "tutorialkit" "$@"
+pnpm build && pnpm publish --recursive --tag "$TAG" --filter "@tutorialkit/*" --filter "tutorialkit" --filter "create-tutorial" "$@"
