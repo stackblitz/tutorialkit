e7206578ac29212cab211f988ea2c8f7dcbe00d1
Dominic Elm <d3lm@users.noreply.github.com>
2024-06-06 15:07:55 +0200

    feat: add create-tutorial package (#47)

---
+++ packages/create-tutorial/src/index.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { spawnSync } from 'node:child_process';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+const tutorialKitEntryPoint = require.resolve('tutorialkit');
+
+spawnSync('node', [tutorialKitEntryPoint, 'create', ...process.argv.slice(2)], { stdio: 'inherit' });
