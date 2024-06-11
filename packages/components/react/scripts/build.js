fdf99ed96958adec5050011291da09e3a3bee757
Sam Verschueren <sam.verschueren@gmail.com>
2024-06-11 09:45:08 +0200

    chore: switch to execa

--- packages/components/react/scripts/build.js
+++ packages/components/react/scripts/build.js
@@ -1,23 +1,10 @@
-import { spawnSync } from 'child_process';
+import { execa } from 'execa';
 import fastGlob from 'fast-glob';
 import { cp } from 'fs/promises';
 import { join } from 'path';
 
-spawnSync('ls', ['node_modules'], { stdio: 'inherit' });
-spawnSync('ls', ['node_modules/.bin'], { stdio: 'inherit' });
-spawnSync('pwd', { stdio: 'inherit' });
-
 // build everything with typescript
-const { status, error } = spawnSync('pnpm', ['exec', 'tsc', '-b'], { stdio: 'inherit' });
-
-if (error) {
-  console.error(error);
-  process.exit(1);
-}
-
-if (status !== 0) {
-  process.exit(status);
-}
+await execa('tsc', ['-b'], { stdio: 'inherit', preferLocal: true });
 
 // copy css files unmodified
 const filePaths = fastGlob.globSync(`./src/**/*.css`, {
