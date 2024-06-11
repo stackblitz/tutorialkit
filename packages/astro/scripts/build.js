44ad07fe3ab10fa16b171e5032050f534d7760b3
Sam Verschueren <sam.verschueren@gmail.com>
2024-06-11 09:05:56 +0200

    chore: try with npx

--- packages/astro/scripts/build.js
+++ packages/astro/scripts/build.js
@@ -9,7 +9,7 @@ import { nodeExternalsPlugin } from 'esbuild-node-externals';
 await rm('dist', { recursive: true, force: true });
 
 // only do typechecking and emit the type declarations with tsc
-spawnSync('../node_modules/.bin/tsc', ['--emitDeclarationOnly', '--project', './tsconfig.build.json'], {
+spawnSync('npx', ['tsc', '--emitDeclarationOnly', '--project', './tsconfig.build.json'], {
   stdio: 'inherit',
 });
 
