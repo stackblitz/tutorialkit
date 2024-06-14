05b1688718ab6e8d7d55c09e892c7f1faef9116e
Sam Verschueren <sam.verschueren@gmail.com>
2024-06-14 15:22:42 +0200

    feat(store): fix current document and add onDocumentChanged (#74)

--- packages/components/react/src/Terminal/index.tsx
+++ packages/components/react/src/Terminal/index.tsx
@@ -1,9 +1,13 @@
 import { FitAddon } from '@xterm/addon-fit';
 import { WebLinksAddon } from '@xterm/addon-web-links';
-import { Terminal as XTerm, type ITheme } from '@xterm/xterm';
+import { Terminal as XTerm } from '@xterm/xterm';
 import '@xterm/xterm/css/xterm.css';
-import { useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
 import { getTerminalTheme } from './theme.js';
+
+export interface TerminalRef {
+  reloadStyles: () => void;
+}
 
 export interface Props {
   theme: 'dark' | 'light';
@@ -13,66 +17,69 @@ export interface Props {
   onTerminalResize?: (cols: number, rows: number) => void;
 }
 
-export function Terminal({ theme, className, readonly = true, onTerminalReady, onTerminalResize }: Props) {
-  const divRef = useRef<HTMLDivElement>(null);
-  const terminalRef = useRef<XTerm>();
+const Terminal = forwardRef<TerminalRef, Props>(
+  ({ theme, className, readonly = true, onTerminalReady, onTerminalResize }, ref) => {
+    const divRef = useRef<HTMLDivElement>(null);
+    const terminalRef = useRef<XTerm>();
 
-  useEffect(() => {
-    if (!divRef.current) {
-      console.error('Terminal reference undefined');
-      return;
-    }
+    useEffect(() => {
+      const element = divRef.current!;
 
-    const element = divRef.current;
+      const fitAddon = new FitAddon();
+      const webLinksAddon = new WebLinksAddon();
 
-    const fitAddon = new FitAddon();
-    const webLinksAddon = new WebLinksAddon();
+      const terminal = new XTerm({
+        cursorBlink: true,
+        convertEol: true,
+        disableStdin: readonly,
+        theme: getTerminalTheme(readonly ? { cursor: '#00000000' } : {}),
+        fontSize: 13,
+        fontFamily: 'Menlo, courier-new, courier, monospace',
+      });
 
-    const terminal = new XTerm({
-      cursorBlink: true,
-      convertEol: true,
-      disableStdin: readonly,
-      theme: getTerminalTheme(readonly ? { cursor: '#00000000' } : {}),
-      fontSize: 13,
-      fontFamily: 'Menlo, courier-new, courier, monospace',
-    });
+      terminalRef.current = terminal;
 
-    terminalRef.current = terminal;
+      terminal.loadAddon(fitAddon);
+      terminal.loadAddon(webLinksAddon);
+      terminal.open(element);
 
-    terminal.loadAddon(fitAddon);
-    terminal.loadAddon(webLinksAddon);
-    terminal.open(element);
-
-    fitAddon.fit();
-
-    const resizeObserver = new ResizeObserver(() => {
       fitAddon.fit();
-      onTerminalResize?.(terminal.cols, terminal.rows);
-    });
 
-    resizeObserver.observe(element);
+      const resizeObserver = new ResizeObserver(() => {
+        fitAddon.fit();
+        onTerminalResize?.(terminal.cols, terminal.rows);
+      });
 
-    onTerminalReady?.(terminal);
+      resizeObserver.observe(element);
 
-    return () => {
-      resizeObserver.disconnect();
-      terminal.dispose();
-    };
-  }, []);
+      onTerminalReady?.(terminal);
 
-  useEffect(() => {
-    if (!terminalRef.current) {
-      return;
-    }
+      return () => {
+        resizeObserver.disconnect();
+        terminal.dispose();
+      };
+    }, []);
 
-    const terminal = terminalRef.current;
+    useEffect(() => {
+      const terminal = terminalRef.current!;
 
-    // we render a transparent cursor in case the terminal is readonly
-    terminal.options.theme = getTerminalTheme(readonly ? { cursor: '#00000000' } : {});
-    terminal.options.disableStdin = readonly;
-  }, [theme, readonly]);
+      // we render a transparent cursor in case the terminal is readonly
+      terminal.options.theme = getTerminalTheme(readonly ? { cursor: '#00000000' } : {});
+      terminal.options.disableStdin = readonly;
+    }, [theme, readonly]);
 
-  return <div className={`h-full ${className}`} ref={divRef} />;
-}
+    useImperativeHandle(ref, () => {
+      return {
+        reloadStyles: () => {
+          const terminal = terminalRef.current!;
+
+          terminal.options.theme = getTerminalTheme(readonly ? { cursor: '#00000000' } : {});
+        },
+      };
+    }, []);
+
+    return <div className={`h-full ${className}`} ref={divRef} />;
+  },
+);
 
 export default Terminal;
