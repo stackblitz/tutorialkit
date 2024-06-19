db233c7b37a787677d205586a88663913e71e99f
Sam Verschueren <sam.verschueren@gmail.com>
2024-06-19 15:29:29 +0200

    docs: add tutorialkit.dev website (#78)

---
+++ docs/tutorialkit.dev/astro.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://tutorialkit.dev',
+  integrations: [
+    react(),
+    starlight({
+      title: 'Create interactive coding tutorials',
+      components: {
+        Head: './src/components/Layout/Head.astro',
+      },
+      customCss: ['./src/styles/custom.scss'],
+      logo: {
+        dark: './src/assets/brand/tutorialkit-logo-dark.svg',
+        light: './src/assets/brand/tutorialkit-logo-light.svg',
+        replacesTitle: true,
+      },
+      plugins: [starlightLinksValidator()],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: 'About',
+              link: '/guides/about/',
+            },
+            {
+              label: 'Installation',
+              link: '/guides/installation/',
+            },
+            {
+              label: 'Creating a Lesson',
+              link: '/guides/creating-content/',
+            },
+            {
+              label: 'Configuration',
+              link: '/guides/configuration/',
+            },
+            {
+              label: 'Deployment',
+              link: '/guides/deployment/',
+            },
+            {
+              label: 'User Interface Reference',
+              link: '/guides/ui/',
+            },
+          ],
+        },
+      ],
+      // social: {
+      // github: 'https://github.com/withastro/starlight',
+      // },
+      tableOfContents: {
+        maxHeadingLevel: 5,
+      },
+    }),
+  ],
+});
