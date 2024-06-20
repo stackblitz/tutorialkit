2b131e597b94671678c2f2e4625e194eb382dab0
Jòan <Nemikolh@users.noreply.github.com>
2024-06-20 15:47:11 +0100

    feat: allow ordering to be config based in addition to folder based (#79)

--- packages/types/src/schemas/part.ts
+++ packages/types/src/schemas/part.ts
@@ -3,6 +3,7 @@ import { baseSchema } from './common.js';
 
 export const partSchema = baseSchema.extend({
   type: z.literal('part'),
+  chapters: z.array(z.string()).optional(),
 });
 
 export type PartSchema = z.infer<typeof partSchema>;
