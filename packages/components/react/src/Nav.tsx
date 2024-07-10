c14b39060de80c0cf6ccaad7da0e6d931d4c38bc
Jòan <Nemikolh@users.noreply.github.com>
2024-07-10 10:55:46 +0000

    fix: remove unused classes

--- packages/components/react/src/Nav.tsx
+++ packages/components/react/src/Nav.tsx
@@ -42,7 +42,7 @@ export function Nav({ lesson: currentLesson, navList }: Props) {
           data-state={`${showDropdown ? 'open' : 'closed'}`}
           className={classNames(
             navStyles.NavContainer,
-            'absolute mx-4 sm:mx-0 z-1 left-0 transition-[background,box-shadow] duration-0 right-0 rounded-[8px] border overflow-hidden z-50',
+            'absolute mx-4 sm:mx-0 z-1 left-0 right-0 rounded-[8px] border overflow-hidden z-50',
           )}
           ref={menuRef}
         >
