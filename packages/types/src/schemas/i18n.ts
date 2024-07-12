09d8bf7bd7673abb5b92b7de569daad1b44b07fd
Jòan <Nemikolh@users.noreply.github.com>
2024-07-12 14:05:04 +0100

    feat: finalize basic i18n support (#133)

--- packages/types/src/schemas/i18n.ts
+++ packages/types/src/schemas/i18n.ts
@@ -29,6 +29,41 @@ export const i18nSchema = z.object({
    * @default 'No preview to run nor steps to show'
    */
   noPreviewNorStepsText: z.string().optional(),
+
+  /**
+   * Text shown on top of the file tree.
+   *
+   * @default 'Files'
+   */
+  filesTitleText: z.string().optional(),
+
+  /**
+   * Text shown on top of the steps section.
+   *
+   * @default 'Preparing Environment'
+   */
+  prepareEnvironmentTitleText: z.string().optional(),
+
+  /**
+   * Text shown for the toggle terminal button.
+   *
+   * @default 'Toggle Terminal'
+   */
+  toggleTerminalButtonText: z.string().optional(),
+
+  /**
+   * Text shown for the solve button.
+   *
+   * @default 'Solve'
+   */
+  solveButtonText: z.string().optional(),
+
+  /**
+   * Text shown for the reset button.
+   *
+   * @default 'Reset'
+   */
+  resetButtonText: z.string().optional(),
 });
 
 export type I18nSchema = z.infer<typeof i18nSchema>;
