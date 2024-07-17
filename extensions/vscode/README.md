33a69f9de5d163029b78133b129147ff23a6de0b
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2024-07-17 15:06:25 +0200

    feat: add vscode extension (#109)

---
+++ extensions/vscode/README.md
@@ -0,0 +1,25 @@
+# TutorialKit Code Extension
+
+**Status**: Preview
+
+## Features
+
+- Viewing lessons, chapters and parts in the side panel
+- Creating new lessons and chapters
+
+<img src="https://github.com/stackblitz/tutorialkit-extension/blob/main/resources/tutorialkit-screenshot.png?raw=true" width="920" alt="TutorialKit screenshot" />
+
+## Installation
+
+1. Open VS Code
+2. Select _Extensions_ in the left vertical toolbar
+3. Search for `tutorialkit`
+4. Click on _Install_
+
+## Description
+
+When designing the content architecture across folders and files, we aimed to make it as plain and clear as possible. However, even the best file system organization doesn't perfectly translate to a tutorial's structure of lessons and chapters.
+
+We wanted to make navigating between the different parts of your courseware or creating new lessons even easier than traversing a file system.
+
+Therefore, we've built a companion Visual Studio Code extension for tutorial authors. The extension visually represents your tutorial structure and allows you to create new chapters and lessons. When you navigate through the displayed structure, the extension automatically focuses on the corresponding folders and files in your file explorer, giving you a quick way to jump to the content you want to edit.
---
+++ extensions/vscode/src/views/lessonsTree.ts
@@ -0,0 +1,166 @@
+import * as fs from 'fs';
+import grayMatter from 'gray-matter';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { cmd } from '../commands';
+import { Lesson } from '../models/Lesson';
+import { getIcon } from '../utils/getIcon';
+
+// import isTutorialKitWorkspace from '../utils/isTutorialKit';
+
+const metadataFiles = ['meta.md', 'meta.mdx', 'content.md', 'content.mdx'];
+export const tutorialMimeType = 'application/tutorialkit.unit';
+
+let lessonsTreeDataProvider: LessonsTreeDataProvider;
+
+export function getLessonsTreeDataProvider() {
+  return lessonsTreeDataProvider;
+}
+export function setLessonsTreeDataProvider(provider: LessonsTreeDataProvider) {
+  lessonsTreeDataProvider = provider;
+}
+
+export class LessonsTreeDataProvider implements vscode.TreeDataProvider<Lesson> {
+  private _lessons: Lesson[] = [];
+  private _isTutorialKitWorkspace = false;
+
+  constructor(
+    private readonly _workspaceRoot: vscode.Uri,
+    private _context: vscode.ExtensionContext,
+  ) {
+    this._loadLessons();
+  }
+
+  private _loadLessons(): void {
+    try {
+      const tutorialFolderPath = vscode.Uri.joinPath(this._workspaceRoot, 'src', 'content', 'tutorial').fsPath;
+      this._isTutorialKitWorkspace = true;
+      this._lessons = this._loadLessonsFromFolder(tutorialFolderPath);
+    } catch {
+      this._isTutorialKitWorkspace = false;
+    }
+  }
+
+  private _loadLessonsFromFolder(folderPath: string): Lesson[] {
+    const lessons: Lesson[] = [];
+    const files = fs.readdirSync(folderPath);
+
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      const stats = fs.statSync(filePath);
+
+      if (stats.isDirectory()) {
+        const lessonName = path.basename(filePath);
+        const subLessons = this._loadLessonsFromFolder(filePath);
+        const lesson = new Lesson(lessonName, filePath, subLessons);
+
+        // check if the folder directly includes one of the metadata files
+        const folderFiles = fs.readdirSync(filePath);
+        const metadataFile = folderFiles.find((folderFile) => metadataFiles.includes(folderFile));
+
+        if (metadataFile) {
+          const metadataFilePath = path.join(filePath, metadataFile);
+          const metadataFileContent = fs.readFileSync(metadataFilePath, 'utf8');
+          const parsedContent = grayMatter(metadataFileContent);
+          lesson.name = parsedContent.data.title;
+          lesson.metadata = {
+            _path: metadataFilePath,
+            ...(parsedContent.data as any),
+          };
+          lessons.push(lesson);
+        }
+      }
+    }
+
+    return lessons;
+  }
+
+  private _onDidChangeTreeData: vscode.EventEmitter<Lesson | undefined> = new vscode.EventEmitter<Lesson | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<Lesson | undefined> = this._onDidChangeTreeData.event;
+
+  refresh(): void {
+    this._loadLessons();
+    this._onDidChangeTreeData.fire(undefined);
+  }
+
+  getTreeItem(lesson: Lesson): vscode.TreeItem {
+    const treeItem = new vscode.TreeItem(lesson.name);
+
+    treeItem.collapsibleState =
+      lesson.children.length > 0 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
+
+    treeItem.contextValue = lesson.metadata?.type;
+
+    const shouldOpenFile = lesson.metadata?.type === 'lesson';
+
+    treeItem.command = {
+      command: cmd.goto.command,
+      title: 'Go to the lesson',
+      arguments: [lesson.path, lesson.metadata, shouldOpenFile],
+    };
+
+    treeItem.iconPath =
+      lesson.metadata?.type === 'lesson' ? getIcon(this._context, 'lesson.svg') : getIcon(this._context, 'chapter.svg');
+
+    return treeItem;
+  }
+
+  getChildren(element?: Lesson): Lesson[] {
+    if (element) {
+      return element.children;
+    }
+
+    return this._lessons;
+  }
+}
+
+export async function useLessonTree() {
+  /**
+   * `
+   * vscode.workspace.onDidChangeWorkspaceFolders((event) => {
+   *   event.added.forEach((folder) => {
+   *     if (isTutorialKitWorkspace(folder)) {
+   *     }
+   *   });
+   * });
+   */
+
+  // vscode.commands.executeCommand('setContext', 'tutorialkit:tree', true);
+
+  cmd.initialize();
+
+  /**
+   * `
+   * const tutorialWorkpaces = (vscode.workspace.workspaceFolders || []).filter(
+   *   isTutorialKitWorkspace,
+   * );
+   * const selectedWorkspace =
+   *   tutorialWorkpaces.length === 1
+   *     ? tutorialWorkpaces[0]
+   *     : await vscode.window
+   *         .showQuickPick(
+   *           tutorialWorkpaces.map((workspace) => workspace.name),
+   *           {
+   *             placeHolder: 'Select a workspace',
+   *           },
+   *         )
+   *         .then((selected) =>
+   *           tutorialWorkpaces.find((workspace) => workspace.name === selected),
+   *         );
+   */
+
+  /**
+   * `
+   * if (selectedWorkspace) {
+   *   setLessonsTreeDataProvider(
+   *     new LessonsTreeDataProvider(selectedWorkspace.uri, context),
+   *   );
+   *   context.subscriptions.push(
+   *     vscode.window.createTreeView('tutorialkit-lessons-tree', {
+   *       treeDataProvider: getLessonsTreeDataProvider(),
+   *       canSelectMany: true,
+   *     }),
+   *   );
+   * }
+   */
+}
