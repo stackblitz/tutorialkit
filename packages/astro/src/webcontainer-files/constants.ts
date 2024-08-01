a0486c33735d1e5b8032062183e2d8b6dda9d680
Ari Perkkiö <ari.perkkio@gmail.com>
2024-08-01 08:06:47 +0300

    fix: ignore templates `node_modules`

--- packages/astro/src/webcontainer-files/constants.ts
+++ packages/astro/src/webcontainer-files/constants.ts
@@ -1,4 +1,4 @@
-export const IGNORED_FILES = ['**/.DS_Store', '**/*.swp'];
+export const IGNORED_FILES = ['**/.DS_Store', '**/*.swp', '**/node_modules/**'];
 export const EXTEND_CONFIG_FILEPATH = '/.tk-config.json';
 export const FILES_FOLDER_NAME = '_files';
 export const SOLUTION_FOLDER_NAME = '_solution';
