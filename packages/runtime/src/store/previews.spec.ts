9f80711408e5de192c23934e403508c3ec327262
Ari Perkkiö <ari.perkkio@gmail.com>
2024-08-12 15:20:46 +0300

    test: single preview

--- packages/runtime/src/store/previews.spec.ts
+++ packages/runtime/src/store/previews.spec.ts
@@ -1,25 +1,51 @@
-import { assert, expect, test, vi } from 'vitest';
+import { assert, expect, test } from 'vitest';
 import { PreviewsStore } from './previews.js';
 import type { PortListener, WebContainer } from '@webcontainer/api';
 
+test("preview is set ready on webcontainer's event", async () => {
+  const { store, emit } = await getStore();
+  store.setPreviews([3000]);
+
+  assert(store.previews.value);
+  expect(store.previews.value[0].ready).toBe(false);
+
+  emit(3000, 'open', 'https://localhost');
+
+  expect(store.previews.value![0].ready).toBe(true);
+});
+
+test('preview is not set ready when different port is ready', async () => {
+  const { store, emit } = await getStore();
+  store.setPreviews([3000]);
+
+  assert(store.previews.value);
+  expect(store.previews.value[0].ready).toBe(false);
+
+  emit(3001, 'open', 'https://localhost');
+
+  expect(store.previews.value[0].ready).toBe(false);
+});
+
 test('marks multiple preview infos ready', async () => {
   const { store, emit } = await getStore();
-
   store.setPreviews([
     { port: 3000, title: 'Dev' },
     { port: 3000, title: 'Docs', pathname: '/docs' },
   ]);
 
+  assert(store.previews.value);
   expect(store.previews.value).toHaveLength(2);
-  expect(store.previews.value![0].ready).toBe(false);
-  expect(store.previews.value![1].ready).toBe(false);
+
+  expect(store.previews.value[0].ready).toBe(false);
+  expect(store.previews.value[0].pathname).toBe(undefined);
+
+  expect(store.previews.value[1].ready).toBe(false);
+  expect(store.previews.value[1].pathname).toBe('/docs');
 
   emit(3000, 'open', 'https://localhost');
 
-  await vi.waitFor(() => {
-    expect(store.previews.value![0].ready).toBe(true);
-    expect(store.previews.value![1].ready).toBe(true);
-  });
+  expect(store.previews.value[0].ready).toBe(true);
+  expect(store.previews.value[1].ready).toBe(true);
 });
 
 async function getStore() {
