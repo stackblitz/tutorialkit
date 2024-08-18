3b480dbd682a8c7657151dc93054f8209fdad312
Lucas Yang <yangchenshin77@gmail.com>
2024-08-18 17:10:29 +0800

    fix(cli): `create-tutorial` not depending on `@tutorialkit/cli` package (#254)

--- packages/create-tutorial/src/index.ts
+++ packages/create-tutorial/src/index.ts
@@ -4,6 +4,6 @@ import { spawnSync } from 'node:child_process';
 import { createRequire } from 'node:module';
 
 const require = createRequire(import.meta.url);
-const tutorialKitEntryPoint = require.resolve('tutorialkit');
+const tutorialKitEntryPoint = require.resolve('@tutorialkit/cli');
 
 spawnSync('node', [tutorialKitEntryPoint, 'create', ...process.argv.slice(2)], { stdio: 'inherit' });
