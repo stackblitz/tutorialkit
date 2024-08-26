6af1bc95fcf101883a9d7a0adbab4403b03cd2ef
Ari Perkkiö <ari.perkkio@gmail.com>
2024-08-26 16:34:16 +0300

    fix: code review

--- packages/theme/src/transition-theme.ts
+++ packages/theme/src/transition-theme.ts
@@ -1,3 +1,5 @@
+// this is a separate module with its own entrypoint so that it can be used in non-Node environment
+
 export const transitionTheme = {
   transitionProperty: 'background-color, border-color, box-shadow',
   transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
