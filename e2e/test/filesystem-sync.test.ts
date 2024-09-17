627c989cfeb63fee221248d952cff3eba0d3beda
Jòan <Nemikolh@users.noreply.github.com>
2024-09-17 08:21:11 +0000

    chore: add test for file in nested folder

--- e2e/test/filesystem-sync.test.ts
+++ e2e/test/filesystem-sync.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 const BASE_URL = '/tests/filesystem-sync';
 
-test('editor should reflect changes made in webcontainer', async ({ page }) => {
+test('editor should reflect changes made from webcontainer', async ({ page }) => {
   const testCase = 'happy-path';
   await page.goto(`${BASE_URL}/${testCase}`);
 
@@ -13,6 +13,23 @@ test('editor should reflect changes made in webcontainer', async ({ page }) => {
   await page.getByTestId('write-to-file').click();
 
   await expect(page.getByRole('textbox', { name: 'Editor' })).toHaveText('Something else', {
+    useInnerText: true,
+  });
+});
+
+test('editor should reflect changes made from webcontainer in file in nested folder', async ({ page }) => {
+  const testCase = 'happy-path';
+  await page.goto(`${BASE_URL}/${testCase}`);
+
+  await page.getByRole('button', { name: 'baz.txt' }).click();
+
+  await expect(page.getByRole('textbox', { name: 'Editor' })).toHaveText('Baz', {
+    useInnerText: true,
+  });
+
+  await page.getByTestId('write-to-file-in-subfolder').click();
+
+  await expect(page.getByRole('textbox', { name: 'Editor' })).toHaveText('Foo', {
     useInnerText: true,
   });
 });
