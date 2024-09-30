b967dac478aaa82fe6b619f774ea901a0aeb4dae
eric-burel <eb@lbke.fr>
2024-09-30 18:21:16 +0200

    rename public image to public asset

--- packages/astro/src/default/utils/logo.ts
+++ packages/astro/src/default/utils/logo.ts
@@ -1,12 +1,12 @@
 import { LOGO_EXTENSIONS } from './constants';
-import { readPublicImage } from './publicImage';
+import { readPublicAsset } from './publicAsset';
 
 export function readLogoFile(logoPrefix: string = 'logo', absolute?: boolean) {
   let logo;
 
   for (const logoExt of LOGO_EXTENSIONS) {
     const logoFilename = `${logoPrefix}.${logoExt}`;
-    logo = readPublicImage(logoFilename, absolute);
+    logo = readPublicAsset(logoFilename, absolute);
 
     if (logo) {
       break;
--- packages/astro/src/default/utils/publicAsset.ts
+++ packages/astro/src/default/utils/publicAsset.ts
@@ -2,15 +2,15 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { joinPaths } from './url';
 
-export function readPublicImage(filename: string, absolute?: boolean) {
-  let image;
+export function readPublicAsset(filename: string, absolute?: boolean) {
+  let asset;
   const exists = fs.existsSync(path.join('public', filename));
 
   if (!exists) {
     return;
   }
 
-  image = joinPaths(import.meta.env.BASE_URL, filename);
+  asset = joinPaths(import.meta.env.BASE_URL, filename);
 
   if (absolute) {
     const site = import.meta.env.SITE;
@@ -19,9 +19,9 @@ export function readPublicImage(filename: string, absolute?: boolean) {
       // the SITE env variable inherits the value from Astro.site configuration
       console.warn('Trying to compute an absolute file URL but Astro.site is not set.');
     } else {
-      image = joinPaths(site, image);
+      asset = joinPaths(site, asset);
     }
   }
 
-  return image;
+  return asset;
 }
