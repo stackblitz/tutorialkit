3125547c043fe4a76dca95b1eb973362967ccf02
Ari Perkkiö <ari.perkkio@gmail.com>
2024-11-25 15:09:35 +0200

    fix(astro): allow URLs in `meta.image` (#422)

--- packages/types/src/schemas/metatags.ts
+++ packages/types/src/schemas/metatags.ts
@@ -8,7 +8,7 @@ export const metaTagsSchema = z.object({
      * Ideally we would want to use `image` from:
      * https://docs.astro.build/en/guides/images/#images-in-content-collections .
      */
-    .describe('A relative path to an image that lives in the public folder to show on social previews.'),
+    .describe('Image to show on social previews. A relative path is resolved to the public folder.'),
   description: z.string().optional().describe('A description for metadata'),
   title: z.string().optional().describe('A title to use specifically for metadata'),
 });
