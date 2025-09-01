6c31d717f4b72a7c8a634ec372a8f155a7c6ecdb
Ari Perkkiö <ari.perkkio@gmail.com>
2025-09-01 08:59:21 +0300

    test: hook timeout on Windows CI (#466)

--- integration/theme-resolving/inline-content.test.ts
+++ integration/theme-resolving/inline-content.test.ts
@@ -9,26 +9,25 @@ const baseDir = path.resolve(__dirname, '../..');
 const cli = path.join(baseDir, 'packages/cli/dist/index.js');
 const tmp = temporaryDirectory();
 
-afterAll(async () => {
-  await fs.rm(tmp, { force: true, recursive: true });
-});
-
-test(
-  'getInlineContentForPackage finds files from @tutorialkit/astro',
-  { timeout: process.env.CI ? 60_000 : 10_000 },
+afterAll(
   async () => {
-    await execa(
-      'node',
-      [cli, 'create', 'theme-test', '--install', '--no-git', '--no-start', '--package-manager', 'pnpm', '--defaults'],
-      { cwd: tmp },
-    );
-
-    const content = getInlineContentForPackage({
-      name: '@tutorialkit/astro',
-      pattern: '/dist/default/**/*.astro',
-      root: `${tmp}/theme-test`,
-    });
-
-    expect(content.length).toBeGreaterThan(0);
+    await fs.rm(tmp, { force: true, recursive: true });
   },
+  process.env.CI ? 60_000 : 10_000,
 );
+
+test('getInlineContentForPackage finds files from @tutorialkit/astro', async () => {
+  await execa(
+    'node',
+    [cli, 'create', 'theme-test', '--install', '--no-git', '--no-start', '--package-manager', 'pnpm', '--defaults'],
+    { cwd: tmp },
+  );
+
+  const content = getInlineContentForPackage({
+    name: '@tutorialkit/astro',
+    pattern: '/dist/default/**/*.astro',
+    root: `${tmp}/theme-test`,
+  });
+
+  expect(content.length).toBeGreaterThan(0);
+});
